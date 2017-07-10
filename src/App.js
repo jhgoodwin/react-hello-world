48e819bd68095cba0a210585303810d94734df4d
John Goodwin <john@jjgoodwin.com>
2017-07-09 23:46:10 -0400

    Added property naem binding support to the HelloWorld component.

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <HelloWorld />
-        <HelloWorld />
+        <HelloWorld name="Jim" />
+        <HelloWorld name="Sally" />
       </div>
     );
   }

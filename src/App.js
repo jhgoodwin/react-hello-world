73c99eb11580f6b37f73b4ca93b5a6e6e15750b6
John Goodwin <john@jjgoodwin.com>
2017-07-11 22:41:11 -0400

    Moved HelloWorld component to a components folder.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './HelloWorld';
+import HelloWorld from './components/HelloWorld/HelloWorld';
 
 class App extends Component {
   render() {

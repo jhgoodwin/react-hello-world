cde33f6254cca921a6a19967c35018306080c5d8
John Goodwin <john@jjgoodwin.com>
2017-07-11 23:34:35 -0400

    Added ability to remove greetings.

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -6,9 +6,13 @@ class HelloWorld extends Component {
         super(props);
         this.state = { greeting: 'Hello' };
         this.frenchify = this.frenchify.bind(this);
+        this.removeGreeting = this.removeGreeting.bind(this);
     }
     frenchify() {
         this.setState({ greeting: 'Bonjour' });
+    }
+    removeGreeting() {
+        this.props.removeGreeting(this.props.name);
     }
     render() {
         return (
@@ -16,6 +20,8 @@ class HelloWorld extends Component {
                 {this.state.greeting} {this.props.name}!
                 <br/>
                 <button onClick={this.frenchify}>Frenchify!</button>
+                <br/>
+                <button onClick={this.removeGreeting}>Remove Me!</button>
             </div>
         );
     }

7dd84cc89ddf2d025b987409d8c12d32718b36ea
John Goodwin <john@jjgoodwin.com>
2017-07-11 23:19:03 -0400

    Moved HelloWorld component back into just components due to pathing issues. Added HelloWorldList component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './components/HelloWorld/HelloWorld';
+import HelloWorldList from './components/HelloWorldList';
 
 class App extends Component {
   render() {
@@ -14,8 +14,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <HelloWorld name="Jim" />
-        <HelloWorld name="Sally" />
+        <HelloWorldList />
       </div>
     );
   }
